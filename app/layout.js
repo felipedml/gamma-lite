9a329c6713839379cf638ca07a7cf2d4009ec223
felipedml <felipedml@yahoo.com.br>
2025-11-05 16:53:51 -0300

    Add global styles and metadata to layout

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+import './globals.css';
 
+export const metadata = {
+  title: 'Gamma-lite',
+  description: 'Gerador de slides com OpenAI + Reveal.js'
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
