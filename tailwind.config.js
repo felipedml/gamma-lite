2005a5b3ca7f0143045293dff7a570ca35d4a06d
felipedml <felipedml@yahoo.com.br>
2025-11-05 19:22:34 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         pc: {
-          // Pembroke Collins
-          primary: "#6D6D6D",   // cinza da marca
+          primary: "#6D6D6D",
           primaryDark: "#5A5A5A",
           primaryLight: "#8B8B8B",
           bg: "#F5F5F5",
           text: "#222222",
           surface: "#FFFFFF",
           accent: "#9E9E9E"
-        },
+        }
       },
       borderRadius: {
         xl: "14px",
       },
       boxShadow: {
         soft: "0 8px 24px rgba(0,0,0,.08)",
-      },
-    },
+      }
+    }
   },
   plugins: [],
-};
+}
